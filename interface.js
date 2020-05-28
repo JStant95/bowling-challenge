74fa7175a4072299ddb5734a315a9756a93c6f43
Jed Stanton <jedstanton@hotmail.com>
2020-05-28 23:36:53 +0100

    jquery for button disappearing added

--- interface.js
+++ interface.js
@@ -16,6 +16,75 @@ $(document).ready(function() {
     }
   }
 
+  function buttonHider(){
+    if(bowl.scorecard[bowl.frame - 1] === undefined){
+      $('#bowl10').show()
+      $('#bowl9').show()
+      $('#bowl8').show()
+      $('#bowl7').show()
+      $('#bowl6').show()
+      $('#bowl5').show()
+      $('#bowl4').show()
+      $('#bowl3').show()
+      $('#bowl2').show()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 1){
+      $('#bowl10').hide()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 2){
+      $('#bowl10').hide()
+      $('#bowl9').hide()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 3){
+      $('#bowl10').hide()
+      $('#bowl9').hide()
+      $('#bowl8').hide()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 4){
+      $('#bowl10').hide()
+      $('#bowl9').hide()
+      $('#bowl8').hide()
+      $('#bowl7').hide()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 5){
+      $('#bowl10').hide()
+      $('#bowl9').hide()
+      $('#bowl8').hide()
+      $('#bowl7').hide()
+      $('#bowl6').hide()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 6){
+      $('#bowl10').hide()
+      $('#bowl9').hide()
+      $('#bowl8').hide()
+      $('#bowl7').hide()
+      $('#bowl6').hide()
+      $('#bowl5').hide()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 7){
+      $('#bowl10').hide()
+      $('#bowl9').hide()
+      $('#bowl8').hide()
+      $('#bowl7').hide()
+      $('#bowl6').hide()
+      $('#bowl5').hide()
+      $('#bowl4').hide()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 8){
+      $('#bowl10').hide()
+      $('#bowl9').hide()
+      $('#bowl8').hide()
+      $('#bowl7').hide()
+      $('#bowl6').hide()
+      $('#bowl5').hide()
+      $('#bowl4').hide()
+      $('#bowl3').hide()
+    } else if(bowl.scorecard[bowl.frame - 1][0] === 9){
+      $('#bowl10').hide()
+      $('#bowl9').hide()
+      $('#bowl8').hide()
+      $('#bowl7').hide()
+      $('#bowl6').hide()
+      $('#bowl5').hide()
+      $('#bowl4').hide()
+      $('#bowl3').hide()
+      $('#bowl2').hide()
+    }
+  }
+
+
   $('#new-game').on('click', function(){
     bowl._reset();
     $("#new-game").hide();
@@ -34,6 +103,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider()
   })
 
   $('#bowl2').on('click', function(){
@@ -41,6 +111,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider()
   })
 
   $('#bowl3').on('click', function(){
@@ -48,6 +119,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider()
   })
 
   $('#bowl4').on('click', function(){
@@ -55,6 +127,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider()
   })
 
   $('#bowl5').on('click', function(){
@@ -62,6 +135,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider()
   })
 
   $('#bowl6').on('click', function(){
@@ -69,6 +143,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider()
   })
 
   $('#bowl7').on('click', function(){
@@ -76,6 +151,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider()
   })
 
   $('#bowl8').on('click', function(){
@@ -83,6 +159,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider();
   })
 
   $('#bowl9').on('click', function(){
@@ -90,6 +167,7 @@ $(document).ready(function() {
     updateScore();
     updateScorecard()
     checkEnd();
+    buttonHider();
   })
 
   $('#bowl10').on('click', function(){
